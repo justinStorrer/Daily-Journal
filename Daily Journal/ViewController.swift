c11abf17000fdc603cb117e336caccaa63edb37f
Justin Storrer <storrerjustin@gmail.com>
2021-04-01 19:45:36 -0400

    Initial Commit

---
+++ Daily Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Journal
+//
+//  Created by Justin Storrer on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
