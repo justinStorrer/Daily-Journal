642ecd2eaf9d72bc3e4f43e95473cf535f0d466a
Justin Storrer <storrerjustin@gmail.com>
2021-04-01 21:00:13 -0400

    Added TableView and second ViewController. Designed an interface.

---
+++ Daily Journal/DailyJournalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DailyJournalViewController.swift
+//  Daily Journal
+//
+//  Created by Justin Storrer on 4/1/21.
+//
+
+import UIKit
+
+class DailyJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
